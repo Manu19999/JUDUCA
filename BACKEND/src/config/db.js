b9a8d0ef4e3a37ddffa90f8b2f022f3adfc33ecb
Manu19999 <148603095+Manu19999@users.noreply.github.com>
2025-02-07 01:04:04 -0600

    ASD
    ASD

--- BACKEND/src/config/db.js
+++ BACKEND/src/config/db.js
@@ -11,9 +11,14 @@ async function conexionbd() {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    connectTimeout: 10000 // 10 segundos
   });
 
   return connection;
 }
+
 
 export default conexionbd;  
