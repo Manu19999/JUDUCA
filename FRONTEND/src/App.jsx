4c608b1250bcf0720a661540941e568d8cdf4e4e
Manu19999 <148603095+Manu19999@users.noreply.github.com>
2025-02-23 23:01:09 -0600

    sad

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <Route path="/crear-evento" element={<CreateEvent />} />
         <Route path="/crearCredencial" element={<CreateCredencial />} />
         <Route path="/confCredencial" element={<ConfigCredencial />} />
-        <Route path="/AsignacionCampos/:idPlantilla" element={<AsignarCampos />} />
+        <Route path="/AsignacionCampos" element={<AsignarCampos />} />
         <Route path="/DiseñadorCredencial" element={<DiseñadorCredencial />} />
 
 
