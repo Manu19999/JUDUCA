b795cedb6da28a0cb200be4abb9b17054f8cec26
Manuel1923 <105465139+Manuel1923@users.noreply.github.com>
2025-03-14 23:27:43 -0600

    Update credencialRoutes.js

--- BACKEND/src/modules/credenciales/routes/credencialRoutes.js
+++ BACKEND/src/modules/credenciales/routes/credencialRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getUbicacionesCampos } from '../controllers/credencialController.js';
+import { ObtenerFichas, ObtenerParticipantesPorFicha, ObtenerCredencialesPorFicha, ObtenerEventos, InsertarCredencial } from '../controllers/credencialController.js';
 
 const router = express.Router();
 
+router.get('/', ObtenerEventos);
+router.get('/fichas', ObtenerFichas);
+router.get('/participantes/:idEvento/:idFichaRegistro', ObtenerParticipantesPorFicha);
+router.get('/credenciales/:idEvento/:idFichaRegistro', ObtenerCredencialesPorFicha);
 
-router.get('/ubicacionescampos', getUbicacionesCampos);
+router.post('/insCredencial', InsertarCredencial);
 
 
 
