e25aaf1c2129ddd3cbd19d0e3e90eaded1e723c4
Manuel1923 <105465139+Manuel1923@users.noreply.github.com>
2025-04-28 00:01:58 -0600

    DELETE FRONT 2.0

--- FRONTEND/src/pages/FormularioFicha.jsx
+++
@@ -1,9 +0,0 @@
-import FormularioFichas from "../components/Eventos/FormularioFicha";
-
-export default function Formularios_Fichas() {
-  return (
-    <div className="container mx-auto p-4">
-      <FormularioFichas />
-    </div>
-  );
-}
--- FRONTEND/src/pages/LlenarFichas.jsx
+++
@@ -1,9 +0,0 @@
-import LlenarFichas from "../components/Eventos/LlenarFichas";
-
-export default function Llena_Fichas() {
-  return (
-    <div className="container mx-auto p-4">
-      <LlenarFichas />
-    </div>
-  );
-}
