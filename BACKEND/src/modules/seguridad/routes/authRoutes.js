4480bb1ac8b3070f5f947a75eaae200f0eb7e124
ortiz-Sam <ortizsam0322@gmail.com>
2025-06-04 02:14:09 -0600

    registro de logout

--- BACKEND/src/modules/seguridad/routes/authRoutes.js
+++ BACKEND/src/modules/seguridad/routes/authRoutes.js
@@ -5,7 +5,7 @@ import checkAuth from '../../../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', Login);
-router.post('/logout', Logout);
+router.post('/logout', checkAuth, Logout);
 
 router.get('/perfil', checkAuth, (req, res) => {
     console.log('Datos del usuario:', req.usuario); // Para depuración en la terminal
